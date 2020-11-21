995862e86a6c2704320305f013ded01848dd8f26
nevakee716 <nevakee716@gmail.com>
2020-11-21 01:59:46 +0100

    Graphic Redo

--- src/layoutSearch.js
+++ src/layoutSearch.js
@@ -9,7 +9,29 @@
     cwApi.registerLayoutForJSActions(this);
     this.errorMessages = [];
     this.javaScriptLoaded = false;
+    this.directiveId = 500;
   };
+
+  var loader = cwApi.CwAngularLoader;
+  if (cwApi.ngDirectives) {
+    cwApi.ngDirectives.push(function () {
+      loader.registerDirective("elemReady", function ($parse) {
+        return {
+          restrict: "A",
+          link: function ($scope, elem, attrs) {
+            $timeout(function () {
+              elem.ready(function () {
+                $scope.$apply(function () {
+                  var func = $parse(attrs.elemReady);
+                  func($scope);
+                });
+              });
+            });
+          },
+        };
+      });
+    });
+  }
 
   cwLayoutSearch.prototype.getContainerId = function () {
     return this.LayoutName + "-" + this.nodeID;
@@ -42,7 +64,10 @@
       this.data[nodeId] = [];
       for (i = 0; i < object[nodeId].length; i += 1) {
         o = object[nodeId][i];
-        o.displayName = this.getItemDisplayString(o);
+        o.displayName = cwAPI.customLibs.utils.getItemDisplayString(this.viewSchema.ViewName, o);
+        var div = document.createElement("div");
+        div.innerHTML = o.displayName;
+        o.sort = div.textContent || div.innerText || "";
         this.data[nodeId].push(o);
       }
     }
@@ -104,37 +129,12 @@
     return cwApi.mapToTranslation(text);
   };
 
-  cwLayoutSearch.prototype.getItemDisplayString = function (item) {
-    var layoutOptions,
-      l,
-      getDisplayStringFromLayout = function (layout) {
-        return layout.getDisplayItem(item);
-      };
-    if (item.nodeID === this.nodeID) {
-      return this.getDisplayItem(item);
-    }
-    if (cwApi.isUndefined(this.layoutsByNodeId)) {
-      this.layoutsByNodeId = {};
-    }
-    if (!this.layoutsByNodeId.hasOwnProperty(item.nodeID)) {
-      if (this.viewSchema.NodesByID.hasOwnProperty(item.nodeID)) {
-        layoutOptions = this.viewSchema.NodesByID[item.nodeID].LayoutOptions;
-        l = new cwApi.cwLayouts[item.layoutName](layoutOptions, this.viewSchema);
-        l.hasTooltip = false;
-        this.layoutsByNodeId[item.nodeID] = l;
-      } else {
-        return item.name;
-      }
-    }
-    return getDisplayStringFromLayout(this.layoutsByNodeId[item.nodeID]);
-  };
-
   cwLayoutSearch.prototype.getTemplatePath = function (folder, templateName) {
     return cwApi.format("{0}/html/{1}/{2}.ng.html", cwApi.getCommonContentPath(), folder, templateName);
   };
 
   cwLayoutSearch.prototype.applyJavaScript = function () {
-    var that = this,
+    var self = this,
       i = 0;
     if (this.javaScriptLoaded) {
       return;
@@ -150,17 +150,17 @@
     cwApi.customLibs.aSyncLayoutLoader.loadUrls(["modules/elasticlunr/elasticlunr.min.js"], function (error) {
       if (error === null) {
         //search engine lib loaded. Create index
-        var searchEngine = new cwApi.customLibs.cwLayoutSearch.searchEngine(that.mmData, that.data);
+        var searchEngine = new cwApi.customLibs.cwLayoutSearch.searchEngine(self.mmData, self.data);
         cwApi.CwAsyncLoader.load("angular", function () {
           var loader = cwApi.CwAngularLoader,
             templatePath,
-            $container = $("#" + that.getContainerId());
+            $container = $("#" + self.getContainerId());
           loader.setup();
-          templatePath = that.getTemplatePath("Search", "templateSearch");
+          templatePath = self.getTemplatePath("Search", "templateSearch");
           loader.loadControllerWithTemplate("cwLayoutSearch", $container, templatePath, function ($scope, $sce, $http) {
-            $scope.containerId = that.nodeID;
-            $scope.scope = that.mmData;
-            $scope.mmDataById = that.mmDataById;
+            $scope.containerId = self.nodeID;
+            $scope.scope = self.mmData;
+            $scope.mmDataById = self.mmDataById;
             $scope.data = [];
             $scope.searchValue = "";
             $scope.options = {
@@ -184,7 +184,7 @@
               return searchEngine.searchItems($scope.searchValue, $scope.scope, $scope.options.search);
             }
             function hasBuildChanged() {
-              var json = JSON.parse(localStorage.getItem("cwLayoutSearch_" + that.nodeID));
+              var json = JSON.parse(localStorage.getItem("cwLayoutSearch_" + self.nodeID));
               if (!cwApi.isUndefinedOrNull(json)) {
                 var hasChanged = json.buildVersion !== cwApi.cwConfigs.DeployNumber;
                 return hasChanged;
@@ -192,43 +192,56 @@
               return true;
             }
             function doSearch() {
-              var i = 0,
-                jsonFile = cwApi.getIndexViewDataUrl(that.viewSchema.ViewName);
+              console.log("Do search");
               disableInput();
               $scope.data = [];
-              // reload data
-              $http.get(jsonFile).then(function (o) {
-                if (cwApi.checkJsonCallback(o)) {
-                  that.data = {};
-                  for (i = 0; i < that.searchNodes.length; i += 1) {
-                    that.getDataFromJson(o.data, that.searchNodes[i]);
-                  }
-                  searchEngine.reloadData(that.data);
-                  $scope.data = getResult();
-                  $scope.data.forEach(function (currentValue, index, arr) {
-                    currentValue.hasResult = currentValue.items.length > 0 ? true : false;
-                    currentValue.isDisplayed = currentValue.items.length > 0 ? true : false;
-                  });
-                  enableInput();
-                  cwApi.tmpSearch = $scope;
-                }
-              }, cwApi.errorOnLoadPage);
+              // reload data (why)
+
+              $scope.data = getResult();
+              $scope.data.forEach(function (currentValue, index, arr) {
+                currentValue.hasResult = currentValue.items.length > 0 ? true : false;
+                currentValue.isDisplayed = currentValue.items.length > 0 ? true : false;
+                currentValue.directive = $scope.containDirective(currentValue.nodeId) ? $scope.getDirective(currentValue) : null;
+                currentValue.objectTypeScriptname = $scope.getObjectTypeScriptnameFromNodeId(currentValue);
+                currentValue.extended = false;
+              });
+              enableInput();
+              cwApi.tmpSearch = $scope;
             }
+            $scope.cwApi = cwApi;
             $scope.toggleOptions = function () {
               $scope.options.isExpanded = !$scope.options.isExpanded;
             };
             $scope.toggleResultNode = function (rNode) {
               rNode.isDisplayed = !rNode.isDisplayed;
             };
+            $scope.cleanUrl = function (url) {
+              return url.replace("<a href='", "").split("'>")[0];
+            };
+            $scope.isFav = function (objectTypeScriptName, object_id) {
+              return cwAPI.customLibs.utils.isObjectFavorite(objectTypeScriptName.toLowerCase(), object_id);
+            };
+            $scope.addAsFav = function (objectTypeScriptName, object_id) {
+              cwAPI.customLibs.utils.addObjectAsFavorite(objectTypeScriptName.toLowerCase(), object_id, function () {
+                $scope.$apply();
+              });
+            };
+
+            $scope.removeAsFav = function (objectTypeScriptName, object_id) {
+              cwAPI.customLibs.utils.removeObjectAsFavorite(objectTypeScriptName.toLowerCase(), object_id, function () {
+                $scope.$apply();
+              });
+            };
+
             $scope.saveOptions = function () {
               localStorage.setItem(
-                "cwLayoutSearch_" + that.nodeID,
+                "cwLayoutSearch_" + self.nodeID,
                 JSON.stringify({ buildVersion: cwApi.cwConfigs.DeployNumber, opt: $scope.options.search, scope: $scope.scope })
               );
               cwApi.notificationManager.addNotification($.i18n.prop("layoutsearch_options_zone_save"));
             };
             $scope.setOptionsFromLocalStorage = function () {
-              var json = JSON.parse(localStorage.getItem("cwLayoutSearch_" + that.nodeID));
+              var json = JSON.parse(localStorage.getItem("cwLayoutSearch_" + self.nodeID));
               if (!cwApi.isUndefinedOrNull(json) && cwApi.cwConfigs.DeployNumber === json.buildVersion) {
                 $scope.options.search.allWords = json.opt.allWords;
                 $scope.options.search.exactMatch = json.opt.exactMatch;
@@ -237,12 +250,10 @@
             };
             $scope.setOptionsFromConfiguration = function () {
               var i = 0,
-                j,
-                node,
-                propertiesScriptName,
-                scopeJson = that.options.CustomOptions["search-scope"];
-              $scope.options.search.exactMatch = that.options.CustomOptions["search-exact-match"];
-              $scope.options.search.allWords = that.options.CustomOptions["search-all-words"];
+                scopeJson = self.options.CustomOptions["search-scope"];
+
+              $scope.options.search.exactMatch = self.options.CustomOptions["search-exact-match"];
+              $scope.options.search.allWords = self.options.CustomOptions["search-all-words"];
               if (scopeJson !== "") {
                 try {
                   scopeJson = JSON.parse(scopeJson);
@@ -251,22 +262,24 @@
                   return;
                 }
                 for (i = 0; i < $scope.scope.length; i += 1) {
-                  node = $scope.scope[i];
-                  if (scopeJson.hasOwnProperty(node.nodeId)) {
-                    propertiesScriptName = scopeJson[node.nodeId].map(function (p) {
-                      return p.toLowerCase();
+                  let nodeScope = $scope.scope[i];
+                  nodeScope.properties.forEach(function (propertyScope, indexPropScope) {
+                    propertyScope.isSelected = scopeJson.some(function (group) {
+                      return group.nodes.some(function (node) {
+                        if (node.nodeId !== nodeScope.nodeId) return false;
+                        node.index = i;
+                        node.label = nodeScope.displayName;
+                        return node.properties.some(function (prop) {
+                          if (propertyScope.scriptName !== prop.scriptname) return false;
+                          prop.index = indexPropScope;
+                          prop.label = propertyScope.displayName;
+                          return prop.selected;
+                        });
+                      });
                     });
-                    for (j = 0; j < node.properties.length; j += 1) {
-                      if (propertiesScriptName.indexOf(node.properties[j].scriptName.toLowerCase()) === -1) {
-                        node.properties[j].isSelected = false;
-                      }
-                    }
-                  } else {
-                    for (j = 0; j < node.properties.length; j += 1) {
-                      node.properties[j].isSelected = false;
-                    }
-                  }
+                  });
                 }
+                $scope.scopeJson = scopeJson;
               }
             };
             $scope.dataHasResult = function (n) {
@@ -279,6 +292,19 @@
             };
             $scope.clickSearch = function () {
               doSearch();
+            };
+            $scope.containDirective = function (nodeId) {
+              let cds = self.viewSchema.NodesByID[nodeId].LayoutOptions.DisplayPropertyScriptName;
+              return cds.indexOf("ngDirective") !== -1;
+            };
+            $scope.getDirective = function (x) {
+              let nodeId = x.nodeId;
+              let cds = self.viewSchema.NodesByID[nodeId].LayoutOptions.DisplayPropertyScriptName;
+              return cds.replace("ngDirective:", "");
+            };
+            $scope.getObjectTypeScriptnameFromNodeId = function (x) {
+              let nodeId = x.nodeId;
+              return self.viewSchema.NodesByID[nodeId].ObjectTypeScriptName;
             };
 
             $scope.displayItemString = function (item) {
